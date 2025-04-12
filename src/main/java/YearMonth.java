a3b149da339c0e17ecccdfb961a388e0a2147313
Boooise <2722940234@qq.com>
2025-04-12 18:01:24 +0800

    测试驱动开发第一个案例

---
+++ src/main/java/YearMonth.java
@@ -0,0 +1,17 @@
+public class YearMonth {
+    private int Year;
+    private int Month;
+
+    public YearMonth(int year, int month) {
+        Year = year;
+        Month = month;
+    }
+
+    public int getYear() {
+        return Year;
+    }
+
+    public int getMonth() {
+        return Month;
+    }
+}
---
+++ src/main/java/YearMonthTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YearMonthTest {
+    @Test
+    public void CreateYearMonthbyInt() {
+        YearMonth yearMonth = new YearMonth(2025, 4);
+        assertEquals(2025, yearMonth.getYear());
+        assertEquals(4, yearMonth.getMonth());
+    }
+
+    @Test
+    public void CreateYearMonthbyInt2() {
+        YearMonth yearMonth = new YearMonth(2026, 5);
+        assertEquals(2026, yearMonth.getYear());
+        assertEquals(5, yearMonth.getMonth());
+    }
+}
